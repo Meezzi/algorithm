e96882a5f361b32805eab18a47b62ed565864c3e
Meezzi <alswl-3016@naver.com>
2025-03-24 11:20:01 +0900

    Time: 2 ms (100%), Space: 189.1 MB (82.44%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.dart
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.dart
@@ -1,12 +1,15 @@
 class Solution {
   int maxProfit(List<int> prices) {
     int minPrice = 1 << 30;
-    int maxProfit = 0;
+    int profit = 0;
 
     for (int price in prices) {
-      minPrice = price < minPrice ? price : minPrice;
-      maxProfit = (price - minPrice) > maxProfit ? (price - minPrice) : maxProfit;
+      if (price < minPrice) {
+        minPrice = price;
+      } else if (profit < price - minPrice) {
+        profit = price - minPrice;
+      }
     }
-    return maxProfit;
+    return profit;
   }
 }
