1db3e457ad40d2f12d5a657eb357e87d40403e7b
Meezzi <alswl-3016@naver.com>
2025-03-25 09:27:28 +0900

    Update 0125-valid-palindrome.dart

--- 0125-valid-palindrome/0125-valid-palindrome.dart
+++ 0125-valid-palindrome/0125-valid-palindrome.dart
@@ -7,6 +7,7 @@ class Solution {
     int first = 0;
     int end = result.length - 1;
 
+    // for문을 이용한 문자열 순회
     // for (int i = 0; i < result.length / 2; i++) {
     //   if (result[i] == result[end]) {
     //     end--;
@@ -15,6 +16,7 @@ class Solution {
     //   }
     // }
 
+    // while 문을 이용한 문자열 순회
     while(first < end) {
       if(result[first] == result[end]) {
         first++;
