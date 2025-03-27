4789c2233f8dd50d31712bdeeedb7feb062af77b
Meezzi <alswl-3016@naver.com>
2025-03-27 09:33:26 +0900

    Time: 1 ms (86.69%), Space: 156.5 MB (16.94%) - LeetHub

--- 0169-majority-element/0169-majority-element.dart
+++ 0169-majority-element/0169-majority-element.dart
@@ -1,27 +1,39 @@
 class Solution {
   int majorityElement(List<int> nums) {
-    nums.sort();
-    int prevNum = 0;
-    int prevCount = 0;
-    int count = 1;
-    int result = 0;
+    // nums.sort();
+    // int prevNum = 0;
+    // int prevCount = 0;
+    // int count = 1;
+    // int result = 0;
 
-    if(nums.length == 1) return nums[0];
+    // if(nums.length == 1) return nums[0];
+
+    // for(int num in nums) {
+    //     if(num != prevNum) {
+    //         prevNum = num;
+    //         count = 1;
+    //     } else {
+    //         count++;
+    //     }
+    //     if(prevCount < count) {
+    //         prevCount = count;
+    //         result = num;
+    //     }
+    // }
+
+    // return result;
+
+    int count = 0;
+    int candidate = nums[0];
 
     for(int num in nums) {
-        if(num != prevNum) {
-            prevNum = num;
-            count = 1;
-        } else {
-            count++;
+        if(count == 0) {
+            candidate = num;
         }
-        
-        if(prevCount < count) {
-            prevCount = count;
-            result = num;
-        }
+
+        count += (num == candidate) ? 1 : -1; 
     }
 
-    return result;
+    return candidate;
   }
 }
