57e57dc5cd3c9b6c53ddb9a6551ed71ea6b66e87
Meezzi <alswl-3016@naver.com>
2025-04-03 22:11:00 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.dart
+++ 0228-summary-ranges/0228-summary-ranges.dart
@@ -1,31 +1,30 @@
-class Solution {
-  List<String> summaryRanges(List<int> nums) {
-    List<String> result = [];
+List<String> summaryRanges(List<int> nums) {
+  // 결과를 저장할 리스트
+  List<String> result = [];
+  int n = nums.length;
 
-    if(nums.length == 0) return result;
- 
-    int start = nums[0];
-    int end = nums[0];
-    for(int i = 1; i < nums.length; i++) {
+  // 배열이 비어있으면 빈 리스트 반환
+  if (n == 0) return result; // 가드 패턴
 
-        if(end + 1 == nums[i]) {
-            end = nums[i];
+  // 범위 시작점
+  int start = nums[0];
 
-        } else {
-            if(start == end) {
-                result.add("$start");
-            } else {
-                result.add("$start->$end");
-            }
-            start = end = nums[i];
-        }
+  for (int i = 0; i < n; i++) {
+    // 마지막 원소이거나 현재 숫자와 다음 숫자가 연속되지 않는 경우
+    if (i == n - 1 || nums[i] + 1 != nums[i + 1]) { 
+      // 범위가 한 숫자인 경우 
+      if (start == nums[i]) {
+        result.add(start.toString());
+      } else {
+        // 범위가 여러 숫자인 경우
+        result.add("$start->${nums[i]}"); 
+      }
+      // 다음 범위의 시작점을 업데이트
+      if (i != n - 1) {
+        start = nums[i + 1];
+      }
     }
-
-    if(start == end) {
-        result.add("$start");
-    } else {
-        result.add("$start->$end");
-    }
-    return result;
   }
+
+  return result;
 }
