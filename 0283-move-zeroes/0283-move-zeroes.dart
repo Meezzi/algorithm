dbe528e4b5325c0c8ae242fe50a62b409f23ed97
Meezzi <alswl-3016@naver.com>
2025-04-07 09:31:34 +0900

    Time: 18 ms (35.33%), Space: 158.8 MB (8.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.dart
+++ 0283-move-zeroes/0283-move-zeroes.dart
@@ -1,11 +1,26 @@
 class Solution {
   void moveZeroes(List<int> nums) {
-    for(int i = 0; i < nums.length; i++) {
-        if(nums[i] == 0) {
-            nums.add(0);
-            nums.remove(nums[i]);
+    // for(int i = 0; i < nums.length; i++) {
+    //     if(nums[i] == 0) {
+    //         // nums.add(0);
+    //         // nums.remove(nums[i]);
+    //     }
+    // }
+
+    int start = 0;
+    int end = 0;
+
+    while (start < nums.length) {
+        if(nums[start] != 0) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            end++;
         }
+        start++;
     }
+
+
     print(nums);
     
   }
