f0de374e73c1a4d8eccffc4ffa8df145b528c820
Meezzi <alswl-3016@naver.com>
2025-04-11 09:39:12 +0900

    Time: 13 ms (55.84%), Space: 152.9 MB (24.68%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.dart
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.dart
@@ -1,30 +1,31 @@
 class Solution {
   String reverseVowels(String s) {
     List<String> strList = s.split('');
-    Set<String> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    List<String> vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 
+    // 투포인터
     int start = 0;
     int end = s.length - 1;
 
-    while (start < end) {
-      // 모음이 아닐 경우 포인터 이동
-      if (!vowels.contains(strList[start])) {
-        start++;
-        continue;
-      }
+    while(start < end) {
+        // 모음이 포함되어 있는지 검사하는 변수
+        while(start < end && !vowels.contains(strList[start])) {
+            start++;
+        }
 
-      if (!vowels.contains(strList[end])) {
-        end--;
-        continue;
-      }
+        while(start < end && !vowels.contains(strList[end])) {
+            end--;
+        }
 
-      // 두 포인터가 모두 모음일 때 스왑
-      String tmp = strList[start];
-      strList[start] = strList[end];
-      strList[end] = tmp;
 
-      start++;
-      end--;
+        if(start < end) {
+            String tmp = strList[start];
+            strList[start] = strList[end];
+            strList[end] = tmp;
+
+            start++;
+            end--;
+        }
     }
 
     return strList.join('');
