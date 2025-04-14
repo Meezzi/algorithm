f6c7f41c3c7f42ddce8f8a49f70bb13bf5039f35
Meezzi <alswl-3016@naver.com>
2025-04-14 09:47:51 +0900

    Time: 1 ms (100%), Space: 151.5 MB (32.95%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.dart
+++ 0383-ransom-note/0383-ransom-note.dart
@@ -22,25 +22,44 @@
 //   }
 // }
 
+// class Solution {
+//   bool canConstruct(String ransomNote, String magazine) {
+//     // 26개 크기의 리스트에 0 저장
+//     // charCount는 magazine에 등장한 알파벳의 횟수 저장
+//     List<int> charCount = List.filled(26, 0);
+
+//     for(int i = 0; i< magazine.length; i++) {
+//         // charCount에 magazine의 알파벳 등장 횟수 저장
+//         // charCount의 index는 알파벳 순서
+//         charCount[magazine.codeUnitAt(i) - 'a'.codeUnitAt(0)]++;
+//     }
+
+//     for(int i = 0; i < ransomNote.length; i++) {
+//         // ransomNote의 현재 알파벳 번호를 index 변수에 저장
+//         int index = ransomNote.codeUnitAt(i) - 'a'.codeUnitAt(0);
+        
+//         // charCount의 알파벳 번호에 해당하는 숫자가 0이라면 false 반환
+//         if(charCount[index] == 0) return false;
+
+//         // charCount의 알파벳 숫자에 해당하는 값을 1 감소
+//         charCount[index]--;
+//     }
+//     return true;
+//   }
+// }
+
 class Solution {
   bool canConstruct(String ransomNote, String magazine) {
-    // 26개 크기의 리스트에 0 저장
     List<int> charCount = List.filled(26, 0);
 
-    for(int i = 0; i< magazine.length; i++) {
-        // 0을 저장했던 charCount에 magazine의 알파벳 등장 횟수 저장
+    for(int i = 0; i < magazine.length; i++) {
         charCount[magazine.codeUnitAt(i) - 'a'.codeUnitAt(0)]++;
     }
 
     for(int i = 0; i < ransomNote.length; i++) {
-        // ransomNote의 현재 알파벳 번호를 index에 저장
         int index = ransomNote.codeUnitAt(i) - 'a'.codeUnitAt(0);
-        
-        // magazine의 알파벳 등장 횟수를 저장한 
-        // charCount의 알파벳 번호에 해당하는 숫자가 0이라면 false 반환
-        if(charCount[index] == 0) return false;
 
-        // charCount의 알파벳 숫자에 해당하는 값을 1 감소
+        if(charCount[index] == 0) return false;
         charCount[index]--;
     }
     return true;
