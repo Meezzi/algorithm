cd00fcc04ad3d95ae3ea6ace29cb2b23c7d38826
Meezzi <alswl-3016@naver.com>
2025-04-21 09:31:19 +0900

    Time: 0 ms (100%), Space: 151.8 MB (28.57%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.dart
+++ 0724-find-pivot-index/0724-find-pivot-index.dart
@@ -1,28 +1,49 @@
+// class Solution {
+//   int pivotIndex(List<int> nums) {
+//     int rightSum = 0;
+//     int leftSum = 0;
+//     int index = 0;
+
+//     while(index < nums.length) {
+
+//         for(int i = 0; i < index; i++) {
+//             leftSum += nums[i];
+//         }
+
+//         for(int i = index + 1; i < nums.length; i++) {
+//             rightSum += nums[i];
+//         }
+
+//         if(rightSum == leftSum) {
+//             return index;
+//         }
+
+//         index++;
+//         rightSum = 0;
+//         leftSum = 0;
+//     }
+
+//     return -1;
+//   }
+// }
+
 class Solution {
-  int pivotIndex(List<int> nums) {
-    int rightSum = 0;
-    int leftSum = 0;
-    int index = 0;
+    int pivotIndex(List<int> nums) {
 
-    while(index < nums.length) {
+        int total = 0;
+        int leftSum = 0;
 
-        for(int i = 0; i < index; i++) {
+        for(int i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(leftSum == total - leftSum - nums[i]) {
+                return i;
+            }
             leftSum += nums[i];
         }
 
-        for(int i = index + 1; i < nums.length; i++) {
-            rightSum += nums[i];
-        }
-
-        if(rightSum == leftSum) {
-            return index;
-        }
-
-        index++;
-        rightSum = 0;
-        leftSum = 0;
+        return -1;
     }
-
-    return -1;
-  }
 }
