18f7ff251980ffdaa0517c6c5e6ac7a63a747e1a
Meezzi <alswl-3016@naver.com>
2025-04-22 10:53:04 +0900

    Time: 0 ms (100%), Space: 149.6 MB (58.33%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.dart
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.dart
@@ -2,16 +2,16 @@ class Solution {
   int minCostClimbingStairs(List<int> cost) {
 
     int n = cost.length;
-    if(n == 2) return cost.reduce((a, b) => a < b ? a : b);
 
-    List<int> dp = List<int>.filled(n, 0);
-    dp[0] = cost[0];
-    dp[1] = cost[1];
+    int prev1 = cost[1];
+    int prev2 = cost[0];
 
     for(int i = 2; i < n; i++) {
-        dp[i] = cost[i] + (dp[i - 1] < dp[i - 2] ? dp[i - 1] : dp[i - 2]);
+        int curr = cost[i] + (prev1 < prev2 ? prev1 : prev2);
+        prev2 = prev1;
+        prev1 = curr;
     }
 
-    return dp[n - 1] < dp[n - 2] ? dp[n - 1] : dp[n - 2];
+    return prev1 < prev2 ? prev1 : prev2;
   }
 }
