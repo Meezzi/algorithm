65981a381cc916db6178d981e58b7ea50770a894
Meezzi <alswl-3016@naver.com>
2025-05-09 12:12:40 +0900

    Time: 12 ms (84.62%), Space: 150.2 MB (63.74%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.dart
@@ -0,0 +1,21 @@
+class Solution {
+  String convert(String s, int numRows) {
+    if (numRows == 1) return s;
+
+    List<StringBuffer> rows = List.generate(numRows, (index) => StringBuffer());
+    int currentRow = 0;
+    bool goingDown = false;
+
+    for (int i = 0; i < s.length; i++) {
+        rows[currentRow].write(s[i]);
+
+        if (currentRow == 0 || currentRow == numRows - 1) {
+        goingDown = !goingDown;
+        }
+
+        currentRow += goingDown ? 1 : -1;
+    }
+
+    return rows.map((row) => row.toString()).join();
+    }
+}
