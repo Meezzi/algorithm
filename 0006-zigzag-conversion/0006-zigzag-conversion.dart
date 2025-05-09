381a1741bbd8508de70bbb93a8af7b5ef37743a3
Meezzi <alswl-3016@naver.com>
2025-05-09 12:24:38 +0900

    Time: 11 ms (94.51%), Space: 149.8 MB (75.82%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.dart
+++ 0006-zigzag-conversion/0006-zigzag-conversion.dart
@@ -3,19 +3,24 @@ class Solution {
     if (numRows == 1) return s;
 
     List<StringBuffer> rows = List.generate(numRows, (index) => StringBuffer());
-    int currentRow = 0;
-    bool goingDown = false;
+
+    int currentRow = 0;       // 현재 작성 중인 행 번호
+    bool goingDown = false;   // 아래 방향으로 진행 중인지 여부
 
     for (int i = 0; i < s.length; i++) {
+        // 현재 행에 문자 추가
         rows[currentRow].write(s[i]);
 
+        // 첫 번째 행이나 마지막 행에 도달하면 방향 전환
         if (currentRow == 0 || currentRow == numRows - 1) {
-        goingDown = !goingDown;
+            goingDown = !goingDown;
         }
 
+        // 방향에 따라 현재 행 번호를 증가 또는 감소
         currentRow += goingDown ? 1 : -1;
     }
 
-    return rows.map((row) => row.toString()).join();
-    }
+    // 각 행의 StringBuffer를 문자열로 변환하고 합쳐서 반환
+    return rows.join('');
+  }
 }
