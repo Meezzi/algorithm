058984cfebbac1a913dc0803723c997e75b5f6c2
Meezzi <alswl-3016@naver.com>
2025-05-14 09:25:27 +0900

    Time: 40 ms (92.16%), Space: 158.2 MB (72.55%) - LeetHub

--- 0015-3sum/0015-3sum.dart
+++ 0015-3sum/0015-3sum.dart
@@ -10,17 +10,18 @@ class Solution {
 
         int left = i + 1;
         int right = nums.length - 1;
+        int target = -nums[i];
 
         while(left < right) {
-            int sum = nums[i] + nums[right] + nums[left];
+            int sum = nums[right] + nums[left];
 
-            if(sum == 0) {
+            if(sum == target) {
                 list.add([nums[i], nums[left], nums[right]]);
                 while(left < right && nums[left] == nums[left + 1]) left++;
                 while(left < right && nums[right] == nums[right - 1]) right--;
                 left++;
                 right--;
-            } else if (sum < 0) {
+            } else if (sum < target) {
                 left++;
             } else {
                 right--;
