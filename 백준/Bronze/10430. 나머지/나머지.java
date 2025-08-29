f30c061f7ab354fa322448412cb36da24d171de3
Meezzi <alswl-3016@naver.com>
2025-08-29 23:46:01 +0900

    [Bronze V] Title: 나머지, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,19 +1,18 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        Scanner sc = new Scanner(System.in);
-        
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
         
         System.out.println((A+B)%C);
-        System.out.println(((A%C)+(B%C))%C);
+        System.out.println(((A%C) + (B%C))%C);
         System.out.println((A*B)%C);
-        System.out.println(((A%C) * (B%C))%C);
-        
-        sc.close();
+        System.out.println((((A%C) * (B%C))%C));
     }
 }
