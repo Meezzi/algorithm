3a40d193ba8e60b1b034d7fb60fd041fd3835a02
Meezzi <alswl-3016@naver.com>
2025-09-03 00:00:32 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1276 ms, Memory: 124028 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -2,35 +2,32 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[m+1][m+1];
-
-        for(int i=1; i<=m; i++) {
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        int[][] sum = new int[N + 1][N + 1];
+        
+        for(int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=1; j<=m; j++) {
-                arr[i][j] = arr[i][j-1] + arr[i-1][j] - arr[i-1][j-1] + Integer.parseInt(st.nextToken());
+            for(int j = 1; j <= N; j++) {
+                int val = Integer.parseInt(st.nextToken());
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + val;
             }
         }
-
-        for(int i=0; i<n; i++) {
+        
+        for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-
-            int sum = arr[x2][y2] - arr[x2][y1-1] - arr[x1-1][y2] + arr[x1-1][y1-1];
-
-            if(x1==x2 && y1==y2)
-                sum = arr[x1][y1] - arr[x1][y1-1] - arr[x1-1][y1] + arr[x1-1][y1-1];
-            if(x1==1 && y1==1 && x2==m && y2==m)
-                sum = arr[x2][y2];
-            System.out.println(sum);
+            
+            System.out.println(sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1]);
         }
+        
     }
 }
